18a4d995995448b6c14b5fc6bafa93720dbeb7a5
Sonali Tandon <sonali_tandon@outlook.com>
2022-03-29 23:11:13 -0700

    Implements mixpanel tracking

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import logo from './logo.svg';
+import mixpanel from 'mixpanel-browser';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+
+mixpanel.init('3298b30c48d1cc9fe3eec5f8c85f3b5d', {debug: true}); 
 
 
 function App() { 
   let [counterValue, setCounterValue] = useState(0);  
 
+  useEffect(() => {
+    const dateTime = new Date();
+    console.log('ran');
+    mixpanel.track('App Loaded', {
+      'Month': dateTime.getMonth() + 1,
+      'Date': dateTime.getDate(),
+      'Year':  dateTime.getFullYear(),
+      'Time': `${dateTime.getHours()}:${dateTime.getMinutes()}:${dateTime.getSeconds()}`,
+    });
+  }, []);
+
   const incrementValue = () => {
-    setCounterValue(counterValue + 1);
+    setCounterValue(++counterValue);
+    mixpanel.track('Button Click', {
+      Action: 'Increment',
+      'Counter Value': counterValue,
+    });
   }
 
   const decrementValue = () => {
-    setCounterValue(counterValue - 1);
+    setCounterValue(--counterValue);
+    mixpanel.track('Button Click', {
+      Action: 'Decrement',
+      'Counter Value': counterValue,
+    });
   }
 
   const resetValue = () => {
     setCounterValue(0);
+    mixpanel.track('Button Click', {
+      Action: 'Reset',
+      'Counter Value': 0,
+    });
   }
 
   return ( 
