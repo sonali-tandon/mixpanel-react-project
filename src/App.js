9c4e14a9e512650513396e0916f0e3a43f256248
Sonali Tandon <sonali_tandon@outlook.com>
2022-03-29 23:52:48 -0700

    Updates CSS and some cleanup

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import mixpanel from 'mixpanel-browser';
 import './App.css';
 import { useEffect, useState } from 'react';
 
-
 mixpanel.init('3298b30c48d1cc9fe3eec5f8c85f3b5d', {debug: true}); 
-
 
 function App() { 
   let [counterValue, setCounterValue] = useState(0);  
 
   useEffect(() => {
     const dateTime = new Date();
-    console.log('ran');
+
     mixpanel.track('App Loaded', {
       'Month': dateTime.getMonth() + 1,
       'Date': dateTime.getDate(),
@@ -22,6 +20,7 @@ function App() {
 
   const incrementValue = () => {
     setCounterValue(++counterValue);
+
     mixpanel.track('Button Click', {
       Action: 'Increment',
       'Counter Value': counterValue,
@@ -30,6 +29,7 @@ function App() {
 
   const decrementValue = () => {
     setCounterValue(--counterValue);
+
     mixpanel.track('Button Click', {
       Action: 'Decrement',
       'Counter Value': counterValue,
@@ -38,6 +38,7 @@ function App() {
 
   const resetValue = () => {
     setCounterValue(0);
+
     mixpanel.track('Button Click', {
       Action: 'Reset',
       'Counter Value': 0,
@@ -46,12 +47,10 @@ function App() {
 
   return ( 
     <div className="app">
-      <div className="counter"> 
-        <button onClick={decrementValue} className="decrement-btn">-</button>
-        <p className="counter-val">{counterValue}</p>
-        <button onClick={incrementValue} className="increment-btn">+</button>
-        <button onClick={resetValue} className="reset-btn">Reset</button>
-      </div>
+      <button onClick={decrementValue} className="decrement-btn">-</button>
+      <p className="counter-val">{counterValue}</p>
+      <button onClick={incrementValue} className="increment-btn">+</button>
+      <button onClick={resetValue} className="reset-btn">RESET</button>
     </div>
   );
 }
